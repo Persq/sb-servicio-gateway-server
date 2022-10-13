8e1c0f45a24e4baea70f3cfb4d8e0da353cc5050
Luis Cochachi <lcochachi@gmail.com>
2022-10-13 18:27:09 -0500

    011

---
+++ src/main/java/com/persq/springboot/app/gateway/security/SpringSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.persq.springboot.app.gateway.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+	
+	@Autowired
+	private JwtAuthenticationFilter authenticationFilter;
+
+	@Bean
+	public SecurityWebFilterChain configure(ServerHttpSecurity http) {
+		return http.authorizeExchange()
+				.pathMatchers("/api/security/oauth/**").permitAll()
+				.pathMatchers(HttpMethod.GET, "/api/productos/listar",
+						"/api/items/listar",
+						"/api/user/usuarios",
+						"/api/items/ver/{id}/cantidad/{cantidad}",
+						"/api/productos/ver/{id}").permitAll()
+				.pathMatchers(HttpMethod.GET, "/api/user/usuarios/{id}").hasAnyRole("ADMIN", "USER")
+				.pathMatchers("/api/productos/**", "/api/items/**", "/api/user/usuarios/**").hasRole("ADMIN")
+				.anyExchange().authenticated()
+				.and().addFilterAt(authenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+				.csrf().disable()
+				.build();
+	}
+	
+}
